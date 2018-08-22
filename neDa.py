a347183eb02b02413291b598b21397a90edc387f
lehats <fst@minus-plus.net>
2018-08-22 18:17:50 +0200

    implement second part of project (most popuar authors)

--- neDa.py
+++ neDa.py
@@ -23,9 +23,37 @@ def get_Top_Articles():
     """Close the connection to teh db. A way to save resources"""     
     conn.close()
 
+def get_Top_Authors():
+
+    """Establish a connection to the db named news using the python module psycopg2. Creates a connection object which is good till it'll be closed"""
+    conn = psycopg2.connect(dbName)
+
+    """create a cursor which is needed to search and fetch data in tables"""
+    cCursor = conn.cursor()
+
+    """Define SQL-query and pass it over to the curser which execute the query and fetch the results"""
+    sQuery = "select name ,sum from authors, ( select sum(count) as sum, author from (select slug, author, count from articles, pathCount where concat('/article/',cast(slug as varchar(20))) like concat('%', cast(path as varchar(20)) , '%')) as newtable group by author) as sumtable where authors.id = sumtable.author order by sum desc;"
+    cCursor.execute(sQuery)
+    results = cCursor.fetchall()
+
+    """Print the results. Before they're printed strings are edited using basic pythin string functions."""
+    print ("\nBelow listed the most popular authors : \n")
+    
+    print (str(results[0][0]) + ': ' + str(results[0][1]) + ' views.')
+    print (str(results[1][0]) + ': ' + str(results[1][1]) + ' views.')
+    print (str(results[2][0]) + ': ' + str(results[2][1]) + ' views.')
+    print (str(results[3][0]) + ': ' + str(results[3][1]) + ' views.\n')
+    
+    
+    """Close the connection to teh db. A way to save resources"""    
+     
+    conn.close()
+
+
+
 
 get_Top_Articles()
 
-
+get_Top_Authors()
 
 

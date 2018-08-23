7fe90fa697d22f71dbd90cd8701ac28f34623d09
lehats <fst@minus-plus.net>
2018-08-23 12:11:53 +0200

    implement third part of project (count errors)

--- neDa.py
+++ neDa.py
@@ -53,10 +53,38 @@ def get_Top_Authors():
     conn.close()
 
 
+def count_Errors():
+
+    
+
+    
+    """Establish a connection to the db named news using the python module psycopg2. Creates a connection object which is good till it'll be closed"""
+    conn = psycopg2.connect(dbName)
+
+    """create a cursor which is needed to search and fetch data in tables"""
+    cCursor = conn.cursor()
+
+    """Define SQL-query and pass it over to the curser which execute the query and fetch the results"""
+    sQuery = "select date, (oneperc*errorCount) as errorPercent from (select date(time) as date2, 100/cast(count(*) as decimal) as onePerc from log group by date2 order by date2) as onePercTable, (select date(time) , count(*) as errorCount from log where status != '200 OK' group by date order by date) as errorCountTable where onePercTable.date2 = errorCountTable.date and onePercTable.oneperc*errorcounttable.errorCount > 1;"
+    cCursor.execute(sQuery)
+    results = cCursor.fetchall()
+
+    """Print the results. Before they're printed strings are edited using basic pythin string functions."""
+    print ("\nBelow listed all dates with more than 1% errors : \n")
+    
+    
+    print (str(results[0][0]) + ': ' + str(round(results[0][1],2)) + '% Errors.\n')
+    
+    
+    
+    """Close the connection to teh db. A way to save resources"""    
+     
+    conn.close()
+
 
 
 get_Top_Articles()
 
 get_Top_Authors()
 
-
+count_Errors()
